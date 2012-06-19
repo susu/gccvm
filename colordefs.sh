fff6a18bed05ce34bb1cf864057222351cc094c3
Marton Suranyi <marton.suranyi@gmail.com>
2012-06-20 00:11:28 +0200

    colorfile was missing

---
+++ colordefs.sh
@@ -0,0 +1,24 @@
+
+NOCOLOR='\e[0m' 
+
+function ec()
+{
+  echo -e $1$2$NOCOLOR
+}
+
+BLACK='\033[1;30m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
+THIN_RED='\033[0;31m'
+THIN_GREEN='\033[0;32m'
+THIN_YELLOW='\033[0;33m'
+THIN_BLUE='\033[0;34m'
+THIN_MAGENTA='\033[0;35m'
+THIN_CYAN='\033[0;36m'
+
