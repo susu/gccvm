23143866a56a1a9748a78073bad6cbfca766bd02
Marton Suranyi <marton.suranyi@gmail.com>
2012-06-20 10:57:02 +0200

    correction for install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 mkdir -pv $HOME/bin
 mkdir -pv $HOME/.gccvm/bin
 
-ln -s colordefs.sh $HOME/bin/colordefs.sh
-ln -s gccvm $HOME/.gccvm/bin/gccvm
+ln -sv `pwd`/colordefs.sh $HOME/bin/colordefs.sh
+ln -sv `pwd`/gccvm $HOME/.gccvm/bin/gccvm
 
 echo "!!! Add this line to your .bashrc manually: "
 echo 'PATH=$HOME/.gccvm/bin/:$PATH'
